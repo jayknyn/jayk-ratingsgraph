41dd4f3c10678c3e3bd683f0a92475e430481e1d
jayknyn <hack@jamminglobal.com>
2019-03-30 17:37:57 -0500

    Updated mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const db = require('../db/db.js');
+const db = require('../db/db.js');
 const port = process.env.port || 3010;
 const app = express();
 app.use(express.static('dist'));
@@ -13,15 +13,15 @@ app.listen(port, (err, success) => {
 app.post('/api/reviews', (req, res) => {
   console.log('app.post started');
   for (let x = 0; x < req.body.length; x++) {
-    // console.log('req.body[x]:', req.body[x]);
+    console.log('req.body[x]:', req.body[x]);
     db.postReview(req.body[x], (err, success) => {
       if (err) {
         console.log('Server: db postReview error')
         res.end();
       } else {
         console.log('Server: db postReview success');
+        res.end('success');
       }
     });
   }
-  res.send('success');
 });
